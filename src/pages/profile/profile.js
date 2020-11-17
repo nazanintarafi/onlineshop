696f09bd0ab750ca4d0166fedfe76ffad5f82b71
nazanintarafi <52406222+nazanintarafi@users.noreply.github.com>
2020-11-17 17:10:34 +0330

    s

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -4,6 +4,7 @@ import ProfileMenu from '../../components/profileMenu/profileMenu';
 import Header from '../../containers/profileHeader/profileHeader';
 import BtnRight from '../../components/UI/button/btnRight/btnRight';
 import UserName from '../../components/ProfileComponents/userName/userName';
+import Button from '../../components/UI/button/button';
 const Profile=(props)=>{
     const [openMenu,setOpenMenu] = useState(false);
     const MenuToggle=(props)=>{
@@ -16,14 +17,15 @@ const Profile=(props)=>{
                 <section className="bodyToggle" style={{
                     position:openMenu?'fixed':'relative',right:openMenu?'50%':'10px',
                     top:openMenu?'90px':'0px',height:openMenu?'390px':'100%',width:openMenu?'100%':'100%',
-                    overflowX:openMenu?'hidden':'hidden',
+                    overflowX:openMenu?'hidden':'hidden',overflowY:openMenu?'hidden':'hidden',
                     transition:openMenu?'all 1s':'all 1s',zIndex:openMenu?'100':"null"}}
                 >
                     <Header rightToggle={openMenu} titre="پروفایل"/>
                     <div className="Content">
                         <UserName />
+                        <Button btnType="submit">ثبت اطلاعات</Button>
                     </div>
-                    <div className="btnRight">
+                    <div className="btnRight" style={{position:openMenu?"absolute":"fixed",right:openMenu?"2px":"12px",transition:openMenu?'all 1s,right 0s':'all 3s,right 0s'}}>
                         <BtnRight clicked={MenuToggle}/>
                     </div>
                 </section>
