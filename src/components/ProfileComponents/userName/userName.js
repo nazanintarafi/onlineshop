49575ebb31571ea9a74334d28ac52ed452feca25
nazanintarafi <52406222+nazanintarafi@users.noreply.github.com>
2020-11-18 11:32:54 +0330

    q

--- src/components/ProfileComponents/userName/userName.js
+++ src/components/ProfileComponents/userName/userName.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Input from '../../Forms/Form/form';
+import "./userName.css";
 const UserName=()=>{
     const[placeHolder,setPlaceholder]=useState(['نام','نام خانوادگی','تاریخ تولد','کد ملی','شماره موبایل','آدرس ایمیل','شماره کارت'])
     return(
@@ -9,9 +10,17 @@ const UserName=()=>{
                 <Input type="text" placeholder="نام خانوادگی" />
                 <Input type="number" placeholder="تاریخ تولد" />
                 <Input type="number" placeholder="کد ملی" />
+                <label className="userNameRadioBtn foreigner">
+                    <input type="radio" value="Foreigner" name="Fareiner" checked="checked" />
+                    <span className="checkmark">تبعه خارجی</span>
+                </label>
                 <Input type="number" placeholder="شماره موبایل" />
                 <Input type="email" placeholder="آدرس ایمیل" />
                 <Input type="number" placeholder="شماره کارت" />
+                <label className="userNameRadioBtn newsletters">
+                    <input type="checkbox" value="share" name="newsletters" checked="checked"/>
+                    <span className="checkmark">اشتراک در خبرنامه آنلاین شاپ</span>
+                </label>
             </form>
         </>
     )
