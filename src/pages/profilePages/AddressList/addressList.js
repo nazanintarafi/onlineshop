3a9c6123e416ae389048a0c93a710030b1736135
nazanintarafi <52406222+nazanintarafi@users.noreply.github.com>
2020-11-21 15:37:37 +0330

    a

--- src/pages/profilePages/AddressList/addressList.js
+++ src/pages/profilePages/AddressList/addressList.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './addressList.css';
 import Body from '../../../components/ProfilePagesBody/profilePagesBody';
 import AddressBox from '../../../components/AddressBox/addressBox';
+
 const AddressList=(props)=>{
     return(
         <>
-            <Body titre="مورد علاقه ها" className="Favorits">
-                <AddressBox />
+            <Body titre="آدرس های ثبت شده" className="Favorits">
+                <AddressBox 
+                    address="تهران، تهران، پاسداران، بهارستان یکم،کوچه نوربخش اول، پلاک 5، طبقه اول"
+                />
+                <AddressBox 
+                    address="تهران، تهران، پاسداران، بهارستان یکم،کوچه نوربخش اول، پلاک 5، طبقه اول"
+                />
+                <AddressBox 
+                    address="تهران، تهران، پاسداران، بهارستان یکم،کوچه نوربخش اول، پلاک 5، طبقه اول"
+                />
             </Body>
             
         </>
