d8098eca3ce1f403fe5a7587a4b767c7ae235650
nazanintarafi <52406222+nazanintarafi@users.noreply.github.com>
2020-11-22 09:49:26 +0330

    a

--- src/pages/profilePages/AddressList/addressList.js
+++ src/pages/profilePages/AddressList/addressList.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './addressList.css';
 import Body from '../../../components/ProfilePagesBody/profilePagesBody';
 import AddressBox from '../../../components/AddressBox/addressBox';
 
 const AddressList=(props)=>{
+    const address =[
+        "تهران، تهران، پاسداران، بهارستان یکم،کوچه نوربخش اول، پلاک 5، طبقه اول",
+        "تهران، تهران، پاسداران، بهارستان یکم،کوچه نوربخش اول، پلاک 5، طبقه اول",
+        "تهران، تهران، پاسداران، بهارستان یکم،کوچه نوربخش اول، پلاک 5، طبقه اول"
+    ]
     return(
         <>
             <Body titre="آدرس های ثبت شده" className="Favorits">
-                <AddressBox 
-                    address="تهران، تهران، پاسداران، بهارستان یکم،کوچه نوربخش اول، پلاک 5، طبقه اول"
-                />
-                <AddressBox 
-                    address="تهران، تهران، پاسداران، بهارستان یکم،کوچه نوربخش اول، پلاک 5، طبقه اول"
-                />
-                <AddressBox 
-                    address="تهران، تهران، پاسداران، بهارستان یکم،کوچه نوربخش اول، پلاک 5، طبقه اول"
-                />
+                address.map((address,index)=>(
+                    <AddressBox 
+                        address={address}
+                    />
+                ))
             </Body>
             
         </>
